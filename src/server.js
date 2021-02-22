b3e810375d50419cb4d58ebfc6fbd42522f91aa3
justhenrique <washington.sousa01@fatec.sp.gov.br>
2021-02-22 07:09:27 -0300

    dynamism with nunjucs

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const server = express();
+
+
 const proffys = [
   {
     name: "Albert Einstein",
@@ -9,23 +13,36 @@ const proffys = [
     weekday: [0],
     time_from: [720],
     time_to: [1220]
-  },
-  {
-    name: "Albert Einstein do Futuro",
-    avatar: "https://www.practicaespanol.com/wp-content/uploads/Alt-Fotograf%C3%ADa-de-Arthur-Sasse-de-la-instant%C3%A1nea-de-Albert-Einstein-EFE.jpg",
-    whatsapp: "12992129200",
-    bio: "Duas coisas são infinitas: o universo e a estupidez humana. Mas, em relação ao universo, ainda não tenho certeza absoluta. Me da uns 10 conto te ensino umas coisas ai sobre a relatividade",
-    subject: "Física",
-    cost: "20",
-    weekday: [0],
-    time_from: [720],
-    time_to: [1220]
   }
 ]
 
+const subjects = [
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação Física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Português",
+  "Química",
+]
 
-const express = require('express');
-const server = express();
+
+function pageLanding (req, res) {
+  return res.render("index.html");
+}
+
+function pageStudy(req, res) {
+  const filters = req.query
+  return res.render("study.html", {proffys, filters});
+}
+
+function pageGiveClass(req, res) {
+  return res.render("give-classes.html");
+}
+
 
 
 // nunjucks
@@ -36,6 +53,7 @@ nunjucks.configure('src/views', {
 });
 
 
+
 server
 .use(express.static("public"))
 
@@ -43,16 +61,3 @@ server
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClass)
 .listen(5500)
-
-
-function pageLanding (req, res) {
-  return res.render("index.html");
-}
-
-function pageStudy(req, res) {
-  return res.render("study.html", {proffy: proffys});
-}
-
-function pageGiveClass(req, res) {
-  return res.render("give-classes.html");
-}
